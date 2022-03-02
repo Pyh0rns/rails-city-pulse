55dc928859c78a39011818f48e6aaa615b5f0d19
Felix Foiret <felix.foiret@outlook.com>
2022-03-02 12:58:18 +0100

    index progress

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,9 @@
 class FavoritesController < ApplicationController
+  def create
+    @favorite = Favorite.new
+    @pulse = Pulse.find(params[:pulse_id])
+    @favorite.pulse = @pulse
+    @favorite.user = current_user
+    @favorite.save
+  end
 end
--- app/controllers/pulses_controller.rb
+++ app/controllers/pulses_controller.rb
@@ -1,2 +1,8 @@
 class PulsesController < ApplicationController
+
+  def index
+    @city = City.find(params[:city_id])
+    @pulses = @city.pulses.all
+  end
+
 end
