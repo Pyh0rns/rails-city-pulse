f2c3f4c4bbee932d89cc8858f74306c686ca85e9
Marin Chalon <marin.4962@gmail.com>
2022-03-02 11:50:01 +0100

    db finished

--- app/models/pulse.rb
+++ app/models/pulse.rb
@@ -1,4 +1,12 @@
 class Pulse < ApplicationRecord
   belongs_to :city
   belongs_to :user
+  has_many :pulse_categories
+  has_many :favorites
+  has_many :categories, through: pulse_categories
+
+  validates :title, length: { minimum: 6 }
+  validates :solution, length: { minimum: 300 }
+  validates :problem, length: { minimum: 150 }
+  validatates :address, presence: true
 end
