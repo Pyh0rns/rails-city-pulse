bb397c8f6d99e881754f95e5886c10b04b9a36ba
Marin Chalon <marin.4962@gmail.com>
2022-03-07 12:37:39 +0100

    card fixed

--- app/models/pulse.rb
+++ app/models/pulse.rb
@@ -13,11 +13,12 @@ class Pulse < ApplicationRecord
   validates :solution, length: { minimum: 3 }
   validates :problem, length: { minimum: 1 }
   validates :address, presence: true
+  # validates :photo, presence: true
 
   has_one_attached :photo
 
   include PgSearch::Model
-  pg_search_scope :global_search, against: [ :title, :solution, :problem],
+  pg_search_scope :global_search, against: [:title, :solution, :problem],
   using: { tsearch: { prefix: true } },
   associated_against: { user: [ :nickname ] }
 
