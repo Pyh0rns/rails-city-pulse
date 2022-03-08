0c968fe1fb72bd7d38f93206f4b1e9363b304165
Marin Chalon <marin.4962@gmail.com>
2022-03-08 18:18:52 +0100

    to fix

--- app/javascript/controllers/pulse_form_controller.js
+++ app/javascript/controllers/pulse_form_controller.js
@@ -4,15 +4,25 @@ export default class extends Controller {
   static targets = [ "form", "firstSection", "secondSection", "thirdSection", "fourthSection", "fifthSecond", "sixthSection", "submitButton" ]
 
   connect() {
-console.log(this.formTarget)
   }
-
   showSecondSection(event) {
-      if (event.keyCode == 13) {
+    if (event.keyCode == 13) {
         this.secondSectionTarget.classList.remove("d-none");
-    console.log("Coucou")
-      }
-
-
+    }
   }
+
+  showThirdSection(event) {
+    if (event.keyCode == 13) {
+      this.thirdSectionTarget.classList.remove("d-none");
+    }
+  }
+
+  showFourthSection(event) {
+    if (event.keyCode == 13) {
+      this.fourthSectionTarget.classList.remove("d-none");
+    }
+  }
+// Find a way to store the value given by geocoder by JS
+// Maybe use the this.formTarget.value into a variable to feed it back to the form later
+// Find a way to fetch the value given by geocoder on this form with this.forInput.value
 }
