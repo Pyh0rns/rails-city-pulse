3c2b21361c456b0ff8dd07a5c3b3085f5fb592a2
Marin Chalon <marin.4962@gmail.com>
2022-03-09 12:27:25 +0100

    ca marche

--- app/javascript/controllers/pulse_form_controller.js
+++ app/javascript/controllers/pulse_form_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "firstInput", "secondInput", "fourthInput", "fifthInput", "sixthInput", "form", "firstSection", "secondSection", "thirdSection", "fourthSection", "geocoderInput", "fifthSecond", "sixthSection", "submitButton" ]
+  static targets = [ "firstInput", "secondInput", "fourthInput", "fifthInput", "sixthInput", "form", "firstSection", "secondSection", "thirdSection", "fourthSection", "geocoderInput", "fifthSection", "sixthSection", "submitButton" ]
 
   connect() {
     this.firstInputTarget.focus();
@@ -20,6 +20,7 @@ export default class extends Controller {
 
   showThirdSection(event) {
     if (event.keyCode == 13) {
+      console.log(this.geocoderInputTarget);
       this.thirdSectionTarget.classList.remove("d-none");
       this.secondInputTarget.blur();
       this.geocoderInputTarget.focus();
@@ -42,14 +43,28 @@ export default class extends Controller {
     if (event.keyCode == 13) {
       this.fifthSectionTarget.classList.remove("d-none");
       this.fourthInputTarget.blur();
-      this.fifthInputTarget.focus();
       this.fifthSectionTarget.scrollIntoView();
     }
   }
+  // Catégories
 
-    // Catégories
+  showSixthSection(event) {
+    if (event.keyCode == 13) {
+      this.sixthSectionTarget.classList.remove("d-none");
+      this.fifthInputTarget.blur();
+      this.sixthInputTarget.focus();
+      this.sixthSectionTarget.scrollIntoView();
+    }
+  }
+
 
     // Submit
+
+    showSubmitButton(event) {
+      if (event.keyCode == 13) {
+        this.submitButtonTarget.classList.remove("d-none");
+      }
+    }
 
 // Find a way to store the value given by geocoder by JS
 // Maybe use the this.formTarget.value into a variable to feed it back to the form later
